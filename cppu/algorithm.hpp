1ec32367d47f2d999e51f0d29680356125937b46
Lingxi-Li <lilingxi.cs@gmail.com>
2016-01-24 11:49:37 +0800

    Improve annotation

--- cppu/algorithm.hpp
+++ cppu/algorithm.hpp
@@ -23,16 +23,14 @@ namespace cppu {
 
 namespace detail {
 
-// performs `swap(x, y)`; lookups in both namespace `std` and that of `T` (by ADL);
-// type of the last parameter makes this overload less fit than `swap(T&, T&, char)`,
-// implementing a preference of `x.swap(y)` over `swap(x, y)`
+// performs `swap(x, y)` and lookups in both namespace `std` and that of `T`
+// (by ADL)
 template <typename T>
 void swap(T& x, T& y, int) {
   cppu_detail_swap::swap_impl(x, y);
 }
 
-// performs `x.swap(y)`; type of the last parameter makes this overload fitter than
-// `swap(T&, T&, int)`, implementing a preference of `x.swap(y)` over `swap(x, y)`
+// performs `x.swap(y)`; disabled if `x.swap(y)` is ill-formed
 template <typename T>
 auto swap(T& x, T& y, char) -> decltype(x.swap(y)) {
   return x.swap(y);
@@ -41,15 +39,19 @@ auto swap(T& x, T& y, char) -> decltype(x.swap(y)) {
 } // namespace detail
 
 /// Introspective swap. Performs `x.swap(y)` if possible. Otherwise, performs
-/// `swap(x, y)` and lookups in both namespace `std` and that of `T`.
+/// `swap(x, y)` and lookups in both namespace `std` and that of `T` (by ADL).
 template <typename T>
 void swap(T& x, T& y) {
+  // the last argument makes `detail::swap(int&, int&, char)`, if enabled, a
+  // better match than `detail::swap(int&, int&, int)`, implementing a
+  // preference of `x.swap(y)` over `swap(x, y)`
   detail::swap(x, y, ' ');
 }
 
-/// Introspective swap. For each pair of elements `x[i]` and `y[i]`, performs
-/// `x[i].swap(y[i])` if possible. Otherwise, performs `swap(x[i], y[i])` and
-/// lookups in both namespace `std` and that of `T`.
+/// Introspective swap for arrays. For each pair of elements `x[i]` and `y[i]`,
+/// performs `x[i].swap(y[i])` if possible. Otherwise, performs
+/// `swap(x[i], y[i])` and lookups in both namespace `std` and that of `T`
+/// (by ADL).
 template <typename T, std::size_t n>
 void swap(T (&x)[n], T (&y)[n]) {
   for (std::size_t i = 0; i < n; ++i) {
--- cppu/string.hpp
+++ cppu/string.hpp
@@ -14,23 +14,23 @@
 namespace cppu {
 
 /// Calls either `to_string(x)` or `to_wstring(x)` based on `CharT`. Lookups in
-/// namespace `std` and that of `T` (by ADL). By providing a uniform name, this
-/// function template facilitates writing generic code.
+/// both namespace `std` and that of `T` (by ADL). By providing a uniform name,
+/// this function template facilitates writing generic code.
 template <typename CharT, typename T,
           typename = std::enable_if_t<std::is_same<CharT, char>::value>>
 std::string to_xstring(const T& x) {
-  // explicitly pulls in `std` in case `T` is not a class type
+  // fallbacks to `std::to_string()` if ADL failed
   using std::to_string;
   return to_string(x);
 }
 
 /// Calls either `to_string(x)` or `to_wstring(x)` based on `CharT`. Lookups in
-/// namespace `std` and that of `T` (by ADL). By providing a uniform name, this
-/// function template facilitates writing generic code.
+/// both namespace `std` and that of `T` (by ADL). By providing a uniform name,
+/// this function template facilitates writing generic code.
 template <typename CharT, typename T,
           typename = std::enable_if_t<std::is_same<CharT, wchar_t>::value>>
 std::wstring to_xstring(const T& x) {
-  // explicitly pulls in `std` in case `T` is not a class type
+  // fallbacks to `std::to_wstring()` if ADL failed
   using std::to_wstring;
   return to_wstring(x);
 }
